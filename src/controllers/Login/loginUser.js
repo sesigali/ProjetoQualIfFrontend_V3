e74b3fa37929f78b8fa3eb3471cd46b6e107c544
sesigali <1232217@isep.ipp.pt>
2024-05-21 18:59:42 +0100

    Update loginUser conexado com o backend

--- src/controllers/Login/loginUser.js
+++ src/controllers/Login/loginUser.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
-import Input from "../../components/Input/input"
-import Button from "../../components/Button/button"
-import '../Login/loginRegisterUser.css'
-import { useNavigate, Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import Input from "../../components/Input/input";
+import Button from "../../components/Button/button";
+import '../Login/loginRegisterUser.css';
+import { useNavigate, Link } from 'react-router-dom';
 import { FaRegWindowClose } from 'react-icons/fa';
-import RecoverUser from './recoverUser'
+import RecoverUser from './recoverUser';
+import axios from 'axios';
 
 export default function LoginUser() {
   const [email, setEmail] = useState('');
@@ -12,46 +13,45 @@ export default function LoginUser() {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:8888/usuario/login', {
+        email: email,
+        senha: senha,
+      });
 
-    if (!email || !senha) {
-      setError('Por favor, forneça um e-mail e uma senha');
-      return;
+      if (response.status === 200) {
+        const userData = response.data;
+        // Redirecionar o usuário para a página inicial ou dashboard
+        navigate('/Home');
+        console.log('Login successful', userData);
+      }
+    } catch (error) {
+      console.error('Error during login', error);
+      setError('E-mail ou senha incorretos. Por favor, tente novamente.');
     }
-
-    if (email === 'email@email.com' && senha === '123') {
-      navigate('/Home');
-
-
-    } else {
-      setError('E-mail ou senha incorretos.')
-    }
-
   };
-
 
   return (
     <div>
-
       <Link to='/'>
         <FaRegWindowClose className='btn-exit1' title='Voltar página principal' />
       </Link>
 
       <div className="container-login">
         <div className="content-login">
-
           <label className="label-login">SISTEMA DE LOGIN</label>
 
           <Input
             type="email"
-            placeholder="Digite seu E-mail:(email@email.com)"
+            placeholder="Digite seu E-mail"
             value={email}
             onChange={(e) => [setEmail(e.target.value), setError("")]}
           />
 
           <Input
-            type="senha"
-            placeholder="Digite sua Senha:(123)"
+            type="password"
+            placeholder="Digite sua Senha"
             value={senha}
             onChange={(e) => [setSenha(e.target.value), setError("")]}
           />
@@ -63,18 +63,13 @@ export default function LoginUser() {
           <label className="label-registrar">
             <span className="span-registrar1">Não tem uma conta?</span>
             <Link to='/register' className='span-registrar2'>
-              <span >&nbsp;Registre-se</span>
+              <span>&nbsp;Registre-se</span>
             </Link>
           </label>
-
         </div>
 
         <RecoverUser />
-
       </div>
-
     </div>
-
-  )
+  );
 }
-//teste
