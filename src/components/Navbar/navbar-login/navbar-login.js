6f17cf4749c3aac62e3ee7c3c1796bb29449230b
Deborafsilva <debora.fsilva@hotmail.com>
2024-09-16 16:05:03 -0300

    Foi algumas alterações no responsivo.

--- src/components/Navbar/navbar-login/navbar-login.js
+++ src/components/Navbar/navbar-login/navbar-login.js
@@ -2,14 +2,31 @@ import { Link } from 'react-router-dom'
 import { FaUser } from 'react-icons/fa';
 import '../../Navbar/navbar.css'
 import Home from '../../../controllers/Home/home';
-
+import { useEffect, useState } from 'react';
 
 export default function NavbarLogin() {
+    const [position, setPosition] = useState(false);
+
+    useEffect(()=> {
+        const handleScroll = () => {
+            if (window.scrollY > 100) { 
+                setPosition(true);
+              } else {
+                setPosition(false);
+              }
+        };
+
+        window.addEventListener('scroll',handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    },[]);
 
     
     return (
 
-        <div className='navbar'>
+        <div className={`navbar ${position ? 'sticky' : ''}`}>
             
             <div >
                 <Link to='/login'>
