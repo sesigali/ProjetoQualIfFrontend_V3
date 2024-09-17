8a6b21ed8dfa1d5237398919e603b8825360fa66
Deborafsilva <debora.fsilva@hotmail.com>
2024-09-17 18:08:53 -0300

    Video Incluido

--- src/controllers/Home/home.js
+++ src/controllers/Home/home.js
@@ -18,12 +18,15 @@ export default function Home() {
                     </p>
                 </div>
                 <div className="video">
-                    <iframe
-                        width="560"
-                        height="315"
-                        src="https://www.youtube.com/embed/video_id"
-                        title="Vídeo Qualif"
-                    />
+                    <iframe 
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/DD3amWHThKk?si=aWNvh0A1XmRaF1h7" title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>
+                    </iframe>
                 </div>
             </div>
             <Footer />
