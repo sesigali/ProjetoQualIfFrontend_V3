8ea44fe99725a14cf93d1506a22c145be0cd0480
Deborafsilva <debora.fsilva@hotmail.com>
2024-11-04 23:20:42 -0300

    Criando o texto de  contato no footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,8 +5,13 @@ export default function Footer() {
 
     return (
         <footer>
-            <div className=''>
+            <div className='text-qualif'>
                 <h1>Qualif - Sistema de Qualificação Econômica-Financeira</h1>
+            </div>
+
+            <div className='contato'>
+                <h1>qualif.contato@gmail.com</h1>
+                {/* <a className='contato' href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=new">qualif.contato@gmail.com</a> */}
             </div>
 
         </footer>
