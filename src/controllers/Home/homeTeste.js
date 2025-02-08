f51f1b3c92fccbf76f1846d8b147b551927c70b7
Deborafsilva <debora.fsilva@hotmail.com>
2025-02-07 22:01:11 -0300

    Atualização dos vídeos do Home e ListaCredor

--- src/controllers/Home/homeTeste.js
+++ src/controllers/Home/homeTeste.js
@@ -17,14 +17,15 @@ export default function HomeTeste() {
                     </p>
                 </div>
                 <div className="video1">
-                    <iframe
-                        width="560"
-                        height="315"
-                        src="https://www.youtube.com/embed/DD3amWHThKk?si=aWNvh0A1XmRaF1h7" title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin"
-                        allowfullscreen>
+                    <iframe 
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/pGykxhx6DsE?si=Aujnt3GOsztbYgLE" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen>
                     </iframe>
                 </div>
             </div>
